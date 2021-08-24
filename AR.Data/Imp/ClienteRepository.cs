e8188580251d9f1484380060e2d56fe265d09851
Guilherme Bandeira <band.92@hotmail.com>
2021-08-24 15:31:22 -0300

    criando controller cliente, adiaicionando tabela cliente e fazendo injeções

---
+++ AR.Data/Imp/ClienteRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AR.Data.Interfaces;
+using AR.Domain;
+
+namespace AR.Data.Imp
+{
+    public class ClienteRepository : IClienteRepository
+    {
+        private readonly ContextoPrincipal _db;
+
+        public ClienteRepository(ContextoPrincipal db)
+        {
+            _db = db;
+        }
+
+        public async Task Add(Cliente entity)
+        {
+            await _db.AddAsync(entity);
+            await  _db.SaveChangesAsync();
+        }
+
+        public IQueryable<Cliente> GetAll()
+        {
+            return _db.Cliente;
+        }
+
+    }
+}
---
+++ AR.Data/Interfaces/IClienteRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AR.Domain;
+
+namespace AR.Data.Interfaces
+{
+    public interface IClienteRepository
+    {
+        IQueryable<Cliente> GetAll();
+        Task Add(Cliente entity);
+    }
+}
